3321c2f8eae32adfd5f4a46d394f99e95c44d52b
raseong <ladol5444@google.com>
2015-03-31 18:20:06 +0900

    과목매니저에서 과목개설을 하여 DAO에서 write를 할 수 있게 수정

--- DI_0318_60112313/src/main/java/Main.java
+++ DI_0318_60112313/src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class HacksaMain {
+public class Main {
 
 
 	public static void main(String[] args) {
@@ -11,20 +11,29 @@ public class HacksaMain {
 		//Professor professor = new Professor("choi");
 		//Lecture lecture = new Lecture("소프트웨어 공학");
 		//Student student = new Student("기라성");
+
+
 		ApplicationContext factory = new ClassPathXmlApplicationContext(
 				new String[]{"applicationContext.xml"});
 		
+		DAO dao = (DAO)factory.getBean("dao");
+		CGwamokManager gwamokManager = (CGwamokManager)factory.getBean("gwamokManager");
+		EGwamok gwamok = (EGwamok)factory.getBean("gwamok");
+	
+		gwamokManager.gaeseol(gwamok);
+		// 과목개설 되었습니다.
+		/* 숙제 : 강의에 학생 추가하는 것
 		Professor professor = (Professor)factory.getBean("professor");
 		Lecture lecture2 = (Lecture)factory.getBean("lecture2");
 		Student student = (Student)factory.getBean("student");
-		
+
 		professor.addLecture(lecture2);
 		professor.showLecture();
 		lecture2.Register(student);
 		lecture2.showStudent();
-		
-		
-		
+		 */
+
+
 	}
 
 }
